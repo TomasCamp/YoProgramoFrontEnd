5c7b4c3437ad2baf788db7a6abbcfa7da998e5c4
Tomas Campanella <tomyecamp@gmail.com>
2022-12-15 23:49:02 -0300

    actualizacion de la maqueta

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+function ocultarBotones() {
+    let btnEdit = document.querySelectorAll(".bi-pencil");
+    btnEdit.forEach(elem =>{
+        elem.parentElement.setAttribute("hidden","true");
+    })
+    let btnTrash = document.querySelectorAll(".bi-trash");
+    btnTrash.forEach(elem =>{
+        elem.parentElement.setAttribute("hidden","true");
+    })
+}
+function mostrarBotones() {
+    let btnEdit = document.querySelectorAll(".bi-pencil");
+    btnEdit.forEach(elem =>{
+        elem.parentElement.removeAttribute("hidden");
+    })
+    let btnTrash = document.querySelectorAll(".bi-trash");
+    btnTrash.forEach(elem =>{
+        elem.parentElement.removeAttribute("hidden");
+    })
+}
+
+ocultarBotones();
+
 const btnIniciar = document.getElementById("IniciarSesion");
 const login = document.querySelector(".login");
+const btnSubmit = document.getElementById("sub");
+const btnCerrar = document.getElementById("CerrarSesion");
 
 window.addEventListener("click", (e)=>{
     if (!login.getAttribute("hidden") && !login.contains(e.target)) {
@@ -8,3 +33,17 @@ window.addEventListener("click", (e)=>{
         login.removeAttribute("hidden");
     }
 });
+
+btnSubmit.addEventListener("click", (e)=>{
+    e.cancelBubble
+    login.setAttribute("hidden", "true");
+    btnIniciar.setAttribute("hidden", "true");
+    btnCerrar.removeAttribute("hidden");
+    mostrarBotones();
+})
+
+btnCerrar.addEventListener("click", ()=>{
+    btnCerrar.setAttribute("hidden", "true");
+    btnIniciar.removeAttribute("hidden");
+    ocultarBotones();
+})
